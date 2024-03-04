764948f1a929e028915db1859ef406a422cdcf24
huynhan03 <huynhnhandanang@gmail.com>
2024-03-04 22:19:34 +0700

    Done menubar

--- Coffee/Coffee/Views/Admin/MainAdminWindow.xaml.cs
+++ Coffee/Coffee/Views/Admin/MainAdminWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -22,6 +23,11 @@ namespace Coffee.Views.Admin
         public MainAdminWindow()
         {
             InitializeComponent();
+        }
+
+        private void Overlay_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            BeginStoryboard((Storyboard)Resources["MenuClose"]);
         }
     }
 }

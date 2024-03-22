72a4036ea0000eb7af949f66b7a95f12a50e275e
huynhan03 <huynhnhandanang@gmail.com>
2024-03-23 00:27:58 +0700

    Add DTO

---
+++ Coffee/Coffee/DTOs/ProductDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffee.DTOs
+{
+    public class ProductDTO
+    {
+        public string MaSanPham { get; set; }
+        public string TenSanPham { get; set; }
+    }
+}

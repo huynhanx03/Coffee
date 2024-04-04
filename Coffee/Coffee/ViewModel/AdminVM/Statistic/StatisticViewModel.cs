36bc0395650fe8c19b3aa490117eb6ba443b76f4
huynhan03 <huynhnhandanang@gmail.com>
2024-04-04 21:03:14 +0700

    Add base statistic

---
+++ Coffee/Coffee/ViewModel/AdminVM/Statistic/StatisticViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffee.ViewModel.AdminVM.Statistic
+{
+    public partial class StatisticViewModel: BaseViewModel
+    {
+        #region
+        #endregion
+
+        #region
+        #endregion
+
+        #region
+        #endregion
+    }
+}
